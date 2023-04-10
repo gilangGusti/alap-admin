ff880a3cca28a233fca65615478329658bc03b55
Gemilang Karunia Gusti <125884682+gilangGusti@users.noreply.github.com>
2023-04-10 21:56:51 +0700

    Create db_alap.sql

---
+++ db_alap.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2023 at 02:05 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_alap`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mengakses`
+--
+-- Error reading structure for table db_alap.mengakses: #1932 - Table 'db_alap.mengakses' doesn't exist in engine
+-- Error reading data for table db_alap.mengakses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_alap`.`mengakses`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengajuan_promo`
+--
+-- Error reading structure for table db_alap.pengajuan_promo: #1932 - Table 'db_alap.pengajuan_promo' doesn't exist in engine
+-- Error reading data for table db_alap.pengajuan_promo: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_alap`.`pengajuan_promo`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengisian_detail`
+--
+-- Error reading structure for table db_alap.pengisian_detail: #1932 - Table 'db_alap.pengisian_detail' doesn't exist in engine
+-- Error reading data for table db_alap.pengisian_detail: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_alap`.`pengisian_detail`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_detail_produk`
+--
+-- Error reading structure for table db_alap.tb_detail_produk: #1932 - Table 'db_alap.tb_detail_produk' doesn't exist in engine
+-- Error reading data for table db_alap.tb_detail_produk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_alap`.`tb_detail_produk`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_produk`
+--
+-- Error reading structure for table db_alap.tb_produk: #1932 - Table 'db_alap.tb_produk' doesn't exist in engine
+-- Error reading data for table db_alap.tb_produk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_alap`.`tb_produk`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_promo`
+--
+-- Error reading structure for table db_alap.tb_promo: #1932 - Table 'db_alap.tb_promo' doesn't exist in engine
+-- Error reading data for table db_alap.tb_promo: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_alap`.`tb_promo`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+-- Error reading structure for table db_alap.tb_transaksi: #1932 - Table 'db_alap.tb_transaksi' doesn't exist in engine
+-- Error reading data for table db_alap.tb_transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_alap`.`tb_transaksi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+-- Error reading structure for table db_alap.tb_user: #1932 - Table 'db_alap.tb_user' doesn't exist in engine
+-- Error reading data for table db_alap.tb_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_alap`.`tb_user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
